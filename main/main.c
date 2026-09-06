bffe954752fef2e5349ccf0ce1ebfab16ce1c048
amekusa03 <amekusa@hotmail.com>
2026-09-07 07:36:05 +0900

    色をRGBに変更

--- main/main.c
+++ main/main.c
@@ -299,7 +299,7 @@ static void init_lcd(void) {
 
     esp_lcd_panel_dev_config_t panel_config = {
         .reset_gpio_num = PIN_NUM_LCD_RST,
-        .rgb_endian = LCD_RGB_ENDIAN_BGR,
+        .rgb_endian = LCD_RGB_ENDIAN_RGB,
         .bits_per_pixel = 16,
     };
     ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(io_handle, &panel_config, &panel_handle));
